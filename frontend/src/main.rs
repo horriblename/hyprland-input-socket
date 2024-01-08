5d84f44f3d6f43e00f73c7d6f16d67369178f9fd
Ching Pei Yang <badnam3o.0@gmail.com>
2024-01-09 00:01:34 +0100

    tried to style window

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,5 +1,4 @@
 use gtk4::{glib, prelude::*, cairo::{Region, RectangleInt}, gdk::DisplayManager};
-use gdk4_wayland::prelude::DisplayExt;
 use gtk4_layer_shell::{LayerShell, Layer};
 use std::{collections::HashMap, os::unix};
 
@@ -25,6 +24,8 @@ impl TouchPoints {
     pub fn remove(&mut self, id: u32) {
         self.points.remove(&id);
     }
+
+    // fn new_circle() -> cairo::path
 }
 
 
@@ -44,12 +45,20 @@ fn build_ui(application: &gtk4::Application) {
     window.init_layer_shell();
     window.set_layer(Layer::Overlay);
 
+    let style = window.style_context();
+    let css_provider = gtk4::CssProvider::new();
+    css_provider.load_from_data("* {
+        background-color: transparent;
+        border: solid 5px red; 
+    }");
+    style.add_provider(&css_provider, gtk4::STYLE_PROVIDER_PRIORITY_APPLICATION);
+
     window.set_title(Some("First GTK Program"));
     window.set_default_size(350, 70);
 
-    let button = gtk4::Button::with_label("Click me!");
+    let canvas = build_canvas();
 
-    window.set_child(Some(&button));
+    window.set_child(Some(&canvas));
 
     window.present();
 
@@ -63,4 +72,21 @@ fn build_ui(application: &gtk4::Application) {
         println!("display does not support input shape");
     }
 
+}
+
+fn build_canvas() -> gtk4::DrawingArea {
+    let canvas = gtk4::DrawingArea::new();
+
+    let style = canvas.style_context();
+    let css_provider = gtk4::CssProvider::new();
+    css_provider.load_from_data("* {background-color: transparent;}");
+    style.add_provider(&css_provider, gtk4::STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    canvas.set_draw_func(|_canvas, cairo_ctx: &gtk4::cairo::Context, _, _| {
+        cairo_ctx.arc(10.0, 10.0, 10.0, 0.0, 2.0 * std::f64::consts::PI);
+        cairo_ctx.set_source_rgba(0.9, 0.9, 0.9, 0.9);
+        _ = cairo_ctx.fill();
+    });
+
+    canvas
 }
